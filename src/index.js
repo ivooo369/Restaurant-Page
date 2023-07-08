7965f324b7679e6e81d8cadbc7a98ee8c823f9d7
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-07-08 23:56:44 +0300

    Make changes to the home page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import { renderHeaderFooter } from './renderHeaderFooter';
+import { renderHomeContent } from './renderHomeContent';
 
 renderHeaderFooter();
+renderHomeContent();
 
