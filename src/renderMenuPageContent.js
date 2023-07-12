0d90f882e2de14f75b457e0bf3dba4c2ca244656
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-07-12 22:17:58 +0300

    Make responsiveness improvements

--- src/renderMenuPageContent.js
+++ src/renderMenuPageContent.js
@@ -2,7 +2,7 @@ import meshana_skara_image from './images/meshana-skara.jpg';
 import tarator_image from './images/tarator.jpg';
 import sarmi_image from './images/sarmi.jpg';
 import shopska_salata_image from './images/shopska-salata.jpg';
-import shkembe_chorba_image from './images/shkembe-chorba.jpg';
+import shkembe_image from './images/shkembe.jpg';
 import moussaka_image from './images/moussaka.jpg';
 import banitsa_image from './images/banitsa.jpg';
 import kiselo_mlyako_image from './images/kiselo-mlyako.jpg';
@@ -32,7 +32,7 @@ export function renderMenuPageContent() {
         { name: 'Tarator', price: 'Price: $1.99', image: tarator_image },
         { name: 'Sarmi', price: 'Price: $2.49', image: sarmi_image },
         { name: 'Shopska Salata', price: 'Price: $3.49', image: shopska_salata_image },
-        { name: 'Shkembe Chorba', price: 'Price: $3.99', image: shkembe_chorba_image },
+        { name: 'Shkembe', price: 'Price: $3.99', image: shkembe_image },
         { name: 'Moussaka', price: 'Price: $3.49', image: moussaka_image },
         { name: 'Banitsa', price: 'Price: $5.49', image: banitsa_image },
         { name: 'Kiselo Mlyako', price: 'Price: $1.49', image: kiselo_mlyako_image }
