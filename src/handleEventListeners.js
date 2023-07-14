a0bcf6681423c06ffb6e3f53638cd2c378ec90c1
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-07-14 20:56:22 +0300

    Fix a bug

--- src/handleEventListeners.js
+++ src/handleEventListeners.js
@@ -7,18 +7,27 @@ export function handleEventListeners() {
     const btnContact = document.querySelector('#btn-contact');
 
     btnHome.addEventListener('click', () => {
+        btnHome.style.textDecoration = 'underline';
+        btnMenu.style.textDecoration = 'none';
+        btnContact.style.textDecoration = 'none';
         homePageContainer.style.display = 'flex';
         menuPageContainer.style.display = 'none';
         contactPageContainer.style.display = 'none';
     });
 
     btnMenu.addEventListener('click', () => {
+        btnMenu.style.textDecoration = 'underline';
+        btnHome.style.textDecoration = 'none';
+        btnContact.style.textDecoration = 'none';
         menuPageContainer.style.display = 'flex';
         homePageContainer.style.display = 'none';
         contactPageContainer.style.display = 'none';
     });
 
     btnContact.addEventListener('click', () => {
+        btnContact.style.textDecoration = 'underline';
+        btnHome.style.textDecoration = 'none';
+        btnMenu.style.textDecoration = 'none';
         contactPageContainer.style.display = 'flex';
         homePageContainer.style.display = 'none';
         menuPageContainer.style.display = 'none';
